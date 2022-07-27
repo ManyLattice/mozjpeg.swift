d529884162dd73dc0f97cc7c3b1edeab1cecb147
Mikhail <manylattice7742@yandex.kz>
2022-07-27 08:44:34 +0600

    Remove tests files

--- Package.swift
+++ Package.swift
@@ -9,18 +9,11 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "mozjpeg",
             targets: ["mozjpeg"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "mozjpeg",
             dependencies: [
@@ -39,9 +32,6 @@ let package = Package(
                 .headerSearchPath("."),
             ],
             linkerSettings: [.linkedFramework("Accelerate")]),
-        .binaryTarget(name: "libturbojpeg", path: "Sources/libturbojpeg.xcframework"),
-        .testTarget(
-            name: "mozjpeg.swiftTests",
-            dependencies: [.target(name: "mozjpeg")]),
+        .binaryTarget(name: "libturbojpeg", path: "Sources/libturbojpeg.xcframework")
     ]
 )
