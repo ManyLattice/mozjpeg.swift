c0c7ee6f092316a24624031f2d49290bb0e56db2
Mikhail <manylattice7742@yandex.kz>
2022-08-01 22:24:21 +0600

    Revert "Update MJEncoder.mm"
    This reverts commit 25141ffc214bbcd1d971f1f71b57d26d912bd5b5.

--- Sources/mozjpegc/MJEncoder.mm
+++ Sources/mozjpegc/MJEncoder.mm
@@ -76,9 +76,7 @@
 }
 
 -(nullable NSData*)compressData:(nonnull NSData*)data size:(MozjpegSize)size quality:(int)quality progressive:(bool)progressive useFastest:(bool)useFastest {
-    NSUInteger len = [data length];
-    Byte *byteData = (Byte*)malloc(len);
-    memcpy(byteData, [data bytes], len);
+    unsigned char* array = (unsigned char*) [data bytes];
     
     const int pixelFormat = TJPF_RGBA;
     
@@ -90,7 +88,7 @@
         flags |= TJFLAG_PROGRESSIVE;
     }
     
-    int result = tjCompress2(tjInstance, static_cast<const unsigned char *>(byteData), static_cast<int>(size.width), 0, static_cast<int>(size.height), pixelFormat, &jpegBuf, &jpegSize, TJSAMP_420, quality, flags);
+    int result = tjCompress2(tjInstance, static_cast<const unsigned char *>(array), static_cast<int>(size.width), 0, static_cast<int>(size.height), pixelFormat, &jpegBuf, &jpegSize, TJSAMP_420, quality, flags);
     
     if (result < 0) {
         tjFree(jpegBuf);
