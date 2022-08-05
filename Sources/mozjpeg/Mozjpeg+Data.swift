bbfe0ee7713aca6216baf165894d592b734f2560
Mikhail <manylattice7742@yandex.kz>
2022-08-06 00:12:07 +0600

    Update Mozjpeg+Data.swift

--- Sources/mozjpeg/Mozjpeg+Data.swift
+++ Sources/mozjpeg/Mozjpeg+Data.swift
@@ -18,17 +18,6 @@ import mozjpegc
 
 public extension Data {
     
-//    func mozjpegRepresentation(size: NSSize, quality: Float) throws -> Data {
-//        let encoder = MJEncoder()
-//        let quality = Swift.max(1, Int32(quality * 100))
-//
-//        guard let data = encoder.compressData(self, size: size, quality: quality, progressive: true, useFastest: false) else {
-//            throw CannotCompressError()
-//        }
-//
-//        return data
-//    }
-    
     func mozjpegRepresentation(size: NSSize, quality: Float) -> Data? {
         guard let imageRep = NSBitmapImageRep(data: self) else { return nil }
         let quality = Swift.max(1, Int32(quality * 100))
